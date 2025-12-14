0a0296c25f6fe4b157ed7b7bc4644c9dc2c43cc7
Saymon Macedo <saymon.macedo@ibm.com>
2025-12-13 23:27:54 -0300

    Changed deploy file and added minio on docker-compose

--- application/backend/src/config/minio.ts
+++ application/backend/src/config/minio.ts
@@ -13,7 +13,7 @@ export class MinioClient {
       endPoint: env.MINIO_HOST,
       port: env.MINIO_PORT,
       useSSL: true,
-      accessKey: env.MINIO_ACESS_KEY,
+      accessKey: env.MINIO_ACCESS_KEY,
       secretKey: env.MINIO_SECRET_KEY,
     })
   }
