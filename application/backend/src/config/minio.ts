fdd9623687dee8713bce238a0e82c8a2641024cd
Saymon Davi De Macedo <126501494+fable00@users.noreply.github.com>
2025-12-13 22:55:26 -0300

    Add minio and fs services

--- application/backend/src/config/minio.ts
+++ application/backend/src/config/minio.ts
@@ -1,9 +1,61 @@
+import { ICreateMemoInput } from '@models/memo'
+import { AppError } from '@shared/app-error'
+import { env } from '@shared/env'
+import { responseFormat } from '@shared/response-format'
 import * as Minio from 'minio'
 
-const minioClient = new Minio.Client({
-  endPoint: 'play.min.io',
-  port: 9000,
-  useSSL: true,
-  accessKey: 'Q3AM3UQ867SPQQA43P2F',
-  secretKey: 'zuf+tfteSlswRu7BJ86wekitnifILbZam1KYY3TG',
-})
+export class MinioClient {
+  private client: Minio.Client;
+  private bucketName = "voice-memo-storage"
+
+  constructor() {
+    this.client = new Minio.Client({
+      endPoint: env.MINIO_HOST,
+      port: env.MINIO_PORT,
+      useSSL: true,
+      accessKey: env.MINIO_ACESS_KEY,
+      secretKey: env.MINIO_SECRET_KEY,
+    })
+  }
+
+  async upload(data: ICreateMemoInput) {    
+    try {  
+      const exists = await this.client.bucketExists(this.bucketName)
+    
+      if (!exists) {
+        await this.client.makeBucket(this.bucketName, "sa-east-1")
+        console.log(responseFormat({
+          statusCode: 200,
+          message: "WARNING! A Bucket was created see if nothing nasty is happening!"
+        }))
+      }
+      
+      const sourceFile: string = data.filePath
+      const objectName: string = `${data.userId}-${Date.now()}-${data.mimetype}`  
+      
+      const metaData = { 
+        'Content-Type': `${data.mimetype}`,
+        'userId': `${data.userId}`,
+        'path': `${data.filePath}`
+      }
+
+      const put = await this.client.fPutObject(this.bucketName, objectName, sourceFile, metaData)
+
+      const response: object = {
+        src: sourceFile,
+        dest: objectName,
+        meta: metaData,
+        etag: put.etag,
+        bucket: this.bucketName
+      }
+
+      return responseFormat({
+        statusCode: 200,
+        message: "Audio Created!",
+        data: response
+      })
+    } catch (err) {
+      throw new AppError(`An error ocurried trying upload a audio: ${err}`, 500)
+    }
+  }
+}
