1cf3a3ed1dba2cf50e4ae438c75853c1da3c1638
Saymon Davi De Macedo <126501494+fable00@users.noreply.github.com>
2025-12-28 15:39:40 -0300

    Adjusting google auth temporarily

--- application/backend/src/shared/google-auth/index.ts
+++ application/backend/src/shared/google-auth/index.ts
@@ -25,7 +25,10 @@ export class GoogleAuth {
 
   static async verifyGoogleToken(tokenCode: string) {
     try {
-      const { tokens } = await googleClient.getToken(tokenCode);
+      const { tokens } = await googleClient.getToken({
+        code: tokenCode, 
+        redirect_uri: 'postmessage'
+      });
       
       if (!tokens.id_token) {
         throw new AppError("Failed to retrieve ID token from Google", 401);
