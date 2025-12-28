2d2854b72b36b07e400ae7babb40c233c65b8b67
Saymon Davi De Macedo <126501494+fable00@users.noreply.github.com>
2025-12-28 14:47:11 -0300

    Update test html and adding routes

--- application/backend/src/routes/index.ts
+++ application/backend/src/routes/index.ts
@@ -1,8 +1,10 @@
 import { Router } from "express";
 import { userRoutes, userUnprotectedRoutes } from "@routes/user-routes";
 import { memoRoutes } from "@routes/memo-routes";
+import { authRoutes } from "./auth-routes";
 
 export const appRouter = Router()
 appRouter.use('/users', userUnprotectedRoutes)
 appRouter.use('/users', userRoutes)
 appRouter.use(`/memos`, memoRoutes)
+appRouter.use('auth', authRoutes)
