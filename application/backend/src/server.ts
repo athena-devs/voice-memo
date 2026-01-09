79e5b49144b0a754b71c7e1a0ef34d132640f41b
fable00 <saymonmcedo00@gmail.com>
2026-01-09 19:33:18 -0300

    Add bootstrap function to await telemetry run then run app and fix compose file

--- application/backend/src/server.ts
+++ application/backend/src/server.ts
@@ -1,10 +1,15 @@
 import { TelemetryClient } from '@config/telemetry-client'
+import { env } from '@shared/env'
+
 const telemetry = new TelemetryClient() 
 telemetry.start()
 
-import { app } from './app'
-import { env } from '@shared/env'
+async function bootstrap() {
+    const { app } = await import('./app')
 
-app.listen(env.PORT, () => { 
-    console.log(`Server is running at http://localhost:${env.PORT}`) 
-})
+    app.listen(env.PORT, () => { 
+        console.log(`Server is running at http://localhost:${env.PORT}`) 
+    })
+}
+
+bootstrap()
